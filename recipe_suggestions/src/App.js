389a3bcacef4be1b4c1527432907728f8a031cc9
Thomas Tran <tat022@ucsd.edu>
2018-07-27 00:05:23 -0700

    added the form and the array handling

--- recipe_suggestions/src/App.js
+++ recipe_suggestions/src/App.js
@@ -1,26 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Background from './Background.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <div class ='background'>
-        <div class ='dark_filter'>
-        </div>
-      </div>
-        <div class= 'title_container'>
-          <div class = 'content'>
-            <h1 class='display-1'>Meal Assist</h1>
-            <form class='button1' action= '/'>
-              <button class= 'btn btn-dark btn-large'>Recipe Suggestion</button>
-            </form>
-            <form class='button2' action='/'>
-              <button class = 'btn btn-dark btn-large'> Grocery Suggestion</button>
-            </form>
-          </div>
-        </div>
+        <Background /> 
       </div>
     );
   }
